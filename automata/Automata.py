39e1870c5d4eac7eae87b3b32ede32bdc4576fd8
Eugene the Red <me@eugenethered.com>
2022-05-26 20:33:54 +0100

    wiring related dependencies

--- automata/Automata.py
+++ automata/Automata.py
@@ -1,4 +1,8 @@
+from typing import Optional
+
+from exchange.InstrumentExchangesHolder import InstrumentExchangesHolder
 from exchangerepo.repository.ExchangeRateRepository import ExchangeRateRepository
+from exchangerepo.repository.InstrumentExchangeRepository import InstrumentExchangeRepository
 from oracle.resolve.PredictionResolver import PredictionResolver
 from positionrepo.repository.PositionRepository import PositionRepository
 from tradestrategy.TradeStrategizor import TradeStrategizor
@@ -11,20 +15,26 @@ class Automata:
     def __init__(self, options):
         self.options = options
         # repositories
-        self.position_repository: PositionRepository = None
-        self.exchange_rate_repository: ExchangeRateRepository = None
+        self.position_repository: Optional[PositionRepository] = None
+        self.instrument_exchange_repository: Optional[InstrumentExchangeRepository] = None
+        self.exchange_rate_repository: Optional[ExchangeRateRepository] = None
         # required dependencies
-        self.prediction_resolver: PredictionResolver = None
-        self.trade_strategizor: TradeStrategizor = None
+        self.prediction_resolver: PredictionResolver = Optional[None]
+        self.trade_strategizor: TradeStrategizor = Optional[None]
+        # data
+        self.instrument_exchanges_holder: InstrumentExchangesHolder = Optional[None]
+        # control init
         self.__init_in_sequence()
 
     def __init_in_sequence(self):
         self.init_repositories()
         self.init_prediction_resolver()
         self.init_trade_strategizor()
+        self.pre_load_data()
 
     def init_repositories(self):
         self.position_repository = PositionRepository(self.options)
+        self.instrument_exchange_repository = InstrumentExchangeRepository(self.options)
         self.exchange_rate_repository = ExchangeRateRepository(self.options)
 
     def init_prediction_resolver(self):
@@ -35,14 +45,19 @@ class Automata:
         if self.trade_strategizor is None:
             raise AutomataRequirementMissingException('Trade Strategizor is required! Implement "init_trade_strategizor"')
 
+    def pre_load_data(self):
+        self.instrument_exchanges_holder = self.instrument_exchange_repository.retrieve()
+
     def run(self):
         position = self.position_repository.retrieve()
 
-        # todo: time frame now
+        instant = position.instant
+        instrument = position.instrument
+        exchanged_from = position.exchanged_from
 
-        # todo: need exchangeable (instruments) + time frame
-        exchange_rates = self.exchange_rate_repository.retrieve_multiple()
+        instrument_exchanges = self.instrument_exchanges_holder.get(instrument)
+        exchange_rates = self.exchange_rate_repository.retrieve_multiple(instrument_exchanges, instrument, instant)
 
-        prediction = self.prediction_resolver.resolve(position.instrument, exchange_rates)
+        prediction = self.prediction_resolver.resolve(instrument, exchange_rates, exchanged_from, instant)
 
         self.trade_strategizor.trade(position, prediction)
--- automata/exception/AutomataRequirementMissingException.py
+++ automata/exception/AutomataRequirementMissingException.py
@@ -1,5 +1,3 @@
 class AutomataRequirementMissingException(Exception):
+    pass
 
-    def __init__(self, error_message):
-        self.error_message = error_message
-        super().__init__(self.error_message)
